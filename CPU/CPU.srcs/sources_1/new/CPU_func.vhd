2f9c911c8e696560963c2fedf0036a9dc4592ead
SverrePoppe <sverre.p.o@gmail.com>
2025-05-17 08:34:31 +0200

    structure

---
+++ CPU/CPU.srcs/sources_1/new/CPU_func.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_BIT.ALL;
+use work.defs_pack.all;
+
+
+entity RISCV is
+end RISCV;
+
+architecture Functional of RISCV is
+
+begin
+
+
+end Functional;
---
+++ CPU/CPU.srcs/sources_1/new/defs_pack.vhd
@@ -0,0 +1,7 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package defs_pack is
+end defs_pack;
+
+
