4af2ce985e687d54bd02e2bd9c79cf14bfa62bd9
vlad-vlkn <vladimir.nikolaev0514@gmail.com>
2025-06-05 16:31:10 +0200

    optimized loads & stores; fixed AUIPC; better readability

--- CPU/CPU.srcs/sources_1/new/CPU_func.vhd
+++ CPU/CPU.srcs/sources_1/new/CPU_func.vhd
@@ -17,7 +17,7 @@ begin
         variable func7        : Func7Type   := (others=>'0');
         variable rs1, rs2, rd : RegAddrType := (others=>'0');
         variable int_rs1, int_rs2, int_rd
-                              : integer := 0;
+                              : natural     := 0;
         variable imm12        : Imm12Type   := (others=>'0');
         variable imm20        : Imm20Type   := (others=>'0');
         variable jimm20       : Imm20Type   := (others=>'0');
@@ -26,73 +26,77 @@ begin
         variable branch_temp  : bit_vector(12 downto 0);
         variable Reg          : RegType     := (others=>(others=>'0'));
         variable Mem          : MemType     := (others=>(others=>'0'));
-        
 
-        begin
-    
-        Instr := Mem(TO_INTEGER(unsigned(PC)));
-        OP := Instr (6 downto 0);
-        func3 := Instr (14 downto 12);
-        func7 := Instr (31 downto 25); -- also used as 1st immediate part for branches and stores
-        rs1   := Instr (19 downto 15);
-        rs2   := Instr (24 downto 20); -- also immediately used for shifts as shamt
-        rd    := Instr (11 downto 7); -- also used as 2nd immediate part for branches and stores
-        imm12 := Instr (31 downto 20);
-        imm20 := Instr (31 downto 12);
-        bImm := Instr(31)&Instr(7)&Instr(30 downto 25)&Instr(11 downto  8);
-        sImm := Instr(31 downto 25)&Instr(11 downto 7);
-        jimm20 := Instr (31)&Instr(19 downto 12)&Instr(20)&Instr(30 downto 21)&'0';
+        variable store_check  : natural     := 0;
+        variable load_address : natural     := 0;
+        
+    begin
+        Instr  := Mem(TO_INTEGER(unsigned(PC)));
+        OP     := Instr(6 downto 0);
+        func3  := Instr(14 downto 12);
+        func7  := Instr(31 downto 25);
+        rs1    := Instr(19 downto 15);
+        rs2    := Instr(24 downto 20); -- also immediately used for shifts as shamt
+        rd     := Instr(11 downto 7);
+        imm12  := Instr(31 downto 20);
+        imm20  := Instr(31 downto 12);
+        bImm   := Instr(31) & Instr(7) & Instr(30 downto 25) & Instr(11 downto  8);
+        sImm   := Instr(31 downto 25) & Instr(11 downto 7);
+        jimm20 := Instr(31) & Instr(19 downto 12) & Instr(20) & Instr(30 downto 21) & '0';
 
         -- Reg is indexed with integers
-        int_rs1   := to_integer(unsigned(rs1));
-        int_rs2   := to_integer(unsigned(rs2));
-        int_rd    := to_integer(unsigned(rd));
+        int_rs1 := to_integer(unsigned(rs1));
+        int_rs2 := to_integer(unsigned(rs2));
+        int_rd  := to_integer(unsigned(rd));
 
         case OP is
-         when OpLoad   =>
-            case func3 is
+            
+            when OpLoad   =>
+                load_address := bit_vector( signed(Reg(int_rs1)) + signed(sign_extend(imm12)) );
+                case func3 is
                     when Func3LB  => -- LB
-                        Reg(bv2natural(rd)) := sign_extend(Mem8( Mem, natural2bv( regAddImm(Reg,rs1,imm12), AddrSize)));
+                        Reg(int_rd) := sign_extend(Mem8( Mem, load_address ));
                     when Func3LH  => -- LH
-                        Reg(bv2natural(rd)) := sign_extend(Mem16( Mem, natural2bv( regAddImm(Reg,rs1,imm12), AddrSize)));
+                        Reg(int_rd) := sign_extend(Mem16( Mem, load_address ));
                     when Func3LW  => -- LW
-                        Reg(bv2natural(rd)) := Mem32( Mem, natural2bv( regAddImm(Reg,rs1,imm12), AddrSize));
+                        Reg(int_rd) :=             Mem32( Mem, load_address );
                     when Func3LBU => -- LBU
-                        Reg(bv2natural(rd)) := zero_extend(Mem8( Mem, natural2bv( regAddImm(Reg,rs1,imm12), AddrSize)));
+                        Reg(int_rd) := zero_extend(Mem8( Mem, load_address ));
                     when Func3LHU => -- LHU
-                        Reg(bv2natural(rd)) := zero_extend(Mem16( Mem, natural2bv( regAddImm(Reg,rs1,imm12), AddrSize)));
+                        Reg(int_rd) := zero_extend(Mem16( Mem, load_address ));
                     when others   =>
                         assert FALSE report "Illegal instruction" severity error;
                 end case;
-        when OpStore  => 
-                
+                    
+            when OpStore  =>
+                store_address := to_integer( signed(Reg(int_rs1)) + signed(sign_extend(sImm)) );
                 case func3 is
                     when Func3SB => -- SB
-                        case regAddImm(Reg,rs1,sImm) mod 4 is -- check the last 2 bits of address
+                        case store_address mod 4 is -- check the last 2 bits of address
                             when 0 => -- Lower half-word (aligned)
-                                Mem( regAddImm(Reg,rs1,sImm) )(7 downto 0) := Reg(bv2natural(rs2))(7 downto 0);
+                                Mem(store_address)(7 downto 0)   := Reg(int_rs2)(7 downto 0);
                             when 1 => -- Upper half-word
-                                Mem( regAddImm(Reg,rs1,sImm) )(15 downto 8) := Reg(bv2natural(rs2))(7 downto 0);
+                                Mem(store_address)(15 downto 8)  := Reg(int_rs2)(7 downto 0);
                             when 2 => -- Lower half-word (aligned)
-                                Mem( regAddImm(Reg,rs1,sImm) )(23 downto 16) := Reg(bv2natural(rs2))(7 downto 0);
+                                Mem(store_address)(23 downto 16) := Reg(int_rs2)(7 downto 0);
                             when 3 => -- Upper half-word
-                                Mem( regAddImm(Reg,rs1,sImm) )(31 downto 24) := Reg(bv2natural(rs2))(7 downto 0);
+                                Mem(store_address)(31 downto 24) := Reg(int_rs2)(7 downto 0);
                             when others =>
                                 assert FALSE report "Unaligned address for SB" severity error;
                         end case;
                     when Func3SH => -- SH
-                        case regAddImm(Reg,rs1,sImm) mod 4 is -- check the last 2 bits of address
+                        case store_address mod 4 is -- check the last 2 bits of address
                             when 0 => -- Lower half-word (aligned)
-                                Mem( regAddImm(Reg,rs1,sImm) )(15 downto 0) := Reg(bv2natural(rs2))(15 downto 0);
+                                Mem(store_address)(15 downto 0)  := Reg(int_rs2)(15 downto 0);
                             when 2 => -- Upper half-word
-                                Mem( regAddImm(Reg,rs1,sImm) )(31 downto 16) := Reg(bv2natural(rs2))(15 downto 0);
+                                Mem(store_address)(31 downto 16) := Reg(int_rs2)(15 downto 0);
                             when others =>
                                 assert FALSE report "Unaligned address for SH" severity error;
                         end case;
                     when Func3SW => -- SW
-                        case regAddImm(Reg,rs1,sImm) mod 4 is -- check the last 2 bits of address
+                        case store_address mod 4 is -- check the last 2 bits of address
                             when 0 =>
-                                Mem( regAddImm(Reg,rs1,sImm) ) := Reg(bv2natural(rs2));
+                                Mem(store_address) := Reg(int_rs2);
                             when others =>
                                 assert FALSE report "Unaligned address for SW" severity error;
                         end case;
@@ -141,7 +145,6 @@ begin
                 when others =>
                     assert FALSE report "Illegal instruction" severity error;
             end case;
-                    
                     
         when OpReg =>
             case func3 is
@@ -215,7 +218,7 @@ begin
         when OpLUI    =>  -- LUI        
                  Reg(int_rd) := imm20 & X"000";
         when OpAUIPC  =>  -- AUIPC
-                 -- Reg(int_rd) := (others => '0'); PROBLEM: [16 bit PC] + [32 bit imm20&X"000"]
+                 Reg(int_rd) := bit_vector( unsigned(PC) + unsigned( (imm20 & X"000")(15 downto 0) ) );
 
         when OpBranch =>
             case func3 is
