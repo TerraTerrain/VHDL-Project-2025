f79faa6d22c9f41cbba23a5e65f4e9c62048f93a
Kate <liudkevych.kate@gmail.com>
2025-07-11 17:03:46 +0200

    Refactor shift entity to use func3 and func7 for operation selection; update definitions in defs_pack.vhd

--- RTL_CPU/ALU/shift/shift.vhd
+++ RTL_CPU/ALU/shift/shift.vhd
@@ -5,25 +5,36 @@ use IEEE.NUMERIC_STD.ALL;
 entity shift is
     Port (
         data_in  : in  DataType;
-        opcode     : in  OpType;
+        func3    : in  Func3Type;
+        func7    : in  Func7Type;
         data_out : out DataType
     );
 end shift;
 
 architecture Behavioral of shift is
 begin
-    process(data_in, opcode)
+    process(data_in, func3, func7)
         variable temp : DataType;
     begin
-        case opcode is
-            when Func7SLL =>
-                temp := data_in sll 1;
-            when Func7SRL => 
-                temp := data_in srl 1;
-            when Func7SRA => 
-                temp := data_in sra 1;
+        case func7 is
+            when Func7ShLog =>
+                case func3 is
+                    when "001" => -- SLL
+                        temp := data_in sll 1;
+                    when "101" => -- SRL
+                        temp := data_in srl 1;                
+                    when others =>
+                        temp := (others => '0');
+                end case;
+            when Func7ShArith =>
+                case func3 is
+                    when "101" => -- SRA
+                        temp := data_in sra 1;              
+                    when others =>
+                        temp := (others => '0');
+                end case;
             when others =>
-                temp := (others => '0');
+                temp := (others => '0'); -- Default
         end case;
         data_out <= temp;
     end process;
