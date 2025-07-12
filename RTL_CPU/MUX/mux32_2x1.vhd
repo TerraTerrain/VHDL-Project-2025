26b786f6ba7c475c420a63daf337f9226af01895
vlad-vlkn <vladimir.nikolaev0514@gmail.com>
2025-07-12 17:22:47 +0200

    mux32_2x1 optimized

--- RTL_CPU/MUX/mux32_2x1.vhd
+++ RTL_CPU/MUX/mux32_2x1.vhd
@@ -1,27 +1,18 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 use WORK.defs_pack.ALL;
 
 entity mux32_2x1 is
-    port ( select_input : in bit;
-            d_in_a, d_in_b  : in DataType;
-            d_out           : out DataType);
+    port(
+        select_input   : in  bit;
+        d_in_a, d_in_b : in  DataType;
+        d_out          : out DataType
+    );
 end mux32_2x1;
 
 architecture RTL of mux32_2x1 is
-
 begin
-
-    process( d_in_a, d_in_b, select_input )
-    
-    begin
-        if select_input = '0' then
-            d_out <= d_in_a;
-        else
-            d_out <= d_in_b;
-        end if;
-    end process;
-    
+    with select_input select
+        d_out <= d_in_a when '0',
+                 d_in_b when '0';
 end RTL;
-
