23fb3c694982d1e655d3e5b43db08061ea722884
vlad-vlkn <vladimir.nikolaev0514@gmail.com>
2025-07-12 21:22:43 +0200

    fixed mux32_4x1.vhd

--- RTL_CPU/MUX/mux32_4x1.vhd
+++ RTL_CPU/MUX/mux32_4x1.vhd
@@ -4,7 +4,7 @@ use WORK.defs_pack.ALL;
 
 entity mux32_4x1 is
     port(
-        select_input   : in  bit_vector(1 downto 0);
+        selector       : in  bit_vector(1 downto 0);
         d_in_a, d_in_b : in  DataType;
         d_in_c, d_in_d : in  DataType;
         d_out          : out DataType
@@ -13,7 +13,7 @@ end mux32_4x1;
 
 architecture RTL of mux32_4x1 is
 begin
-    with select_input select
+    with selector select
         d_out <= d_in_a when "00",
                  d_in_b when "01",
                  d_in_c when "10",
